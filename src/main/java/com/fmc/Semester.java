56cc727db9c018b2a90fcea8da54d315c36d92d1
fatmaQunnies <fatima.n.qunnies@gmail.com>
2023-12-15 00:56:29 +0200

    fix errors

--- src/main/java/com/fmc/Semester.java
+++ src/main/java/com/fmc/Semester.java
@@ -2,16 +2,19 @@ package com.fmc;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Semester extends Course{
    
-    
+    ArrayList<Course> c=new ArrayList<>();// c يعني semesterCourseArray
+
 private int year;
 private LocalDate start;
 private LocalDate end;
-
+public Semester(){}
 
 public Semester(int year, LocalDate start, LocalDate end) {
     super();
@@ -19,6 +22,7 @@ public Semester(int year, LocalDate start, LocalDate end) {
     this.start = start;
     this.end = end;
 }
+
 
 public int getYear() {
     return year;
@@ -54,24 +58,20 @@ public ArrayList<Course> getCourse() {
 public void createNewCourseinSemester(String name,String shortcut, int credits, Faculty faculty){
 
     Course course = new Course(name, shortcut,credits,this, faculty );
-    this.c.add(this);
+    this.c.add(course);
     this.semesterCourse.put(this, c);
   // اتأكد انها شغالة
 }
 public void removeCourse(Course course){
-    courses.remove(course);
+ this.c.remove(course);
+  
 }
 
-public void removeCourse(String course){
+public void removeCourse(String Namecourse){
    
-List<String> remove = courses.stream().map(e -> e.getName().toLowerCase()).filter(e -> e.equals(course.toLowerCase())).collect(Collectors.toList());
-courses.remove(remove);
+List<String> removes = this.c.stream().map(e -> e.getName().toLowerCase()).filter(e -> e.equals(Namecourse.toLowerCase())).collect(Collectors.toList());
+this.c.remove(removes);//اتأكد اذا هي شغالةة
 }
-
-
-
-
-
 
 
 
--- src/main/java/com/fmc/Staff.java
+++ src/main/java/com/fmc/Staff.java
@@ -5,18 +5,21 @@ import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Staff  extends Student{
-    
+   ArrayList<Course> staffCourseArray=new ArrayList<>();
+
 private String name;
 private String email;
 private long phoneNumber;
-private ArrayList<Course> courses;
-public Staff(String name, String email, long phoneNumber, ArrayList<Course> courses) {
+
+
+public Staff(String name, String email, long phoneNumber) {
     this.name = name;
     this.email = email;
     this.phoneNumber = phoneNumber;
-    this.courses = courses;
+    
 }
 public String getName() {
     return name;
@@ -36,32 +39,41 @@ public long getPhoneNumber() {
 public void setPhoneNumber(long phoneNumber) {
     this.phoneNumber = phoneNumber;
 }
-public ArrayList<Course> getCourse() {///جاهزة
-    List<Course>getCourses= courses.stream().filter(e->e.getStaff().equals(this)).collect(Collectors.toList());
-    return (ArrayList<Course>) getCourses;
+public ArrayList<Course> getCourse(String staffName) {///جاهزة
+  ArrayList<Course>st=staffCourse.get(this);
+    return st;
+    // List<Course>getCourses= st.stream().filter(e->e.getStaff().equals(this)).collect(Collectors.toList());
+    //  (ArrayList<Course>) getCourses;
 }
 public void setCourse(Course course) {
-    this.courses.add(course);
+    ArrayList<Course> temp =staffCourse.get(this);
+    temp.add(course);
+    staffCourse.put(this, temp);
+   
 }
-public void assignGrade(Student student,Course course,Double grade ) throws InterruptedException{// لازم اتأكد انو المادة موجودة جاهزة
-    int testCours=(int) this.getCourse().stream().filter(e->e.equals(course)).count();
-    ArrayList<Student>cour=course.getStudent();
-    int test =(int) cour.stream().filter(e->e.equals(student)).count();
-    if(test!=0&&testCours!=0){
-ArrayBlockingQueue<Student> gpas=new ArrayBlockingQueue<>(2);
-student.setGrade(course, grade);
-gpas.put(student);}
-else{
-    System.out.println("not exist");
-}
+// public void assignGrade(Student student,Course course,Double grade ) throws InterruptedException{// لازم اتأكد انو المادة موجودة جاهزة
+    
+//     int testCours=(int)( Stream.of(staffCourse).filter(e->e.equals(course)).count());
+//     ArrayList<Student>cour=course.getStudent();
+//     int test =(int) cour.stream().filter(e->e.equals(student)).count();
+//     if(test!=0&&testCours!=0){
+// ArrayBlockingQueue<Student> gpas=new ArrayBlockingQueue<>(2);
+// student.setGrade(course, grade) ;
+// gpas.put(student);}
+// else{
+//     System.out.println("not exist");
+// }
 
-}
-
-
-
+// }
 
 
 
 
+public ArrayList<Course> getCourse() {
+    return staffCourseArray;
+ }
+ public void addCourse(Course course1){
+    staffCourseArray.add(course1);
+ }
 
 }
